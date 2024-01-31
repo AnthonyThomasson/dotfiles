1a5326c9a363c7180f23e5bf22ede611ae86a3bb
Anthony Thomasson <athomasson.93@gmail.com>
2024-01-30 18:18:04 -0700

    Check required dependencies before installing

--- install.sh
+++ install.sh
@@ -1,3 +1,26 @@
 export DOTFILES_PATH=$PWD
 
+echo "🔍 Verifying installer dependencies..."
+isMissingDependency=false
+if command -v curl >/dev/null 2>&1; then
+	echo "✅ curl installed"
+else
+	echo "❌ curl not installed"
+	isMissingDependency=true
+fi
+if command -v git >/dev/null 2>&1; then
+	echo "✅ git installed"
+else
+	echo "❌ git not installed"
+	isMissingDependency=true
+fi
+
+if $isMissingDependency; then
+	echo "❌ git not installed"
+	exit 1
+else
+	echo "✅ verified installer dependencies"
+fi
+
+
 sh ./zsh/install.sh
